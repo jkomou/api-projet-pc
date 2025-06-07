6fab55a640a9d80ae39d0d532a97b6465bb75115
Komou Jean <jlkomou@outlook.com>
2025-06-07 18:49:25 +0200

    Connection avec MongoDB

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose'); 
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -10,3 +11,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Server app listening on port ' + port);
 });
+
+mongoose.connect('mongodb+srv://admin:admin123@cluster0.v4ut44m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', 
+     { useNewUrlParser: true, 
+     useUnifiedTopology: true }) 
+     .then(() => console.log('Connexion à MongoDB réussie !')) 
+     .catch(() => console.log('Connexion à MongoDB échouée !')); 
+    
