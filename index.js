de97288e06823b917ee8cd214da1e1a19e5790e9
Komou Jean <jlkomou@outlook.com>
2025-06-18 11:26:46 +0200

    Importation des premières données pour les collections

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose'); 
 const app = express();
 const port = process.env.PORT || 8000;
+const dbPassword = process.env.DB_PASSWORD;
+const dbUser = process.env.DB_USER;
+
 
 app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
@@ -12,7 +16,7 @@ app.listen(port, () => {
     console.log('Server app listening on port ' + port);
 });
 
-mongoose.connect('mongodb+srv://admin:admin123@cluster0.v4ut44m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', 
+mongoose.connect('mongodb+srv://' + dbUser + ':' + dbPassword + '@cluster0.v4ut44m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', 
      { useNewUrlParser: true, 
      useUnifiedTopology: true }) 
      .then(() => console.log('Connexion à MongoDB réussie !')) 
---
+++ routes/composants.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const component = require('components');
+
+// GET : liste de tous les composants avec filtres
+router.get('/', (req, res) => {
+    const { categorie, marque } = req.query;
+    let resultats = composants;
+  
+    if (categorie) resultats = resultats.filter(c => c.categorie_id === categorie);
+    if (marque) resultats = resultats.filter(c => c.marque === marque);
+  
+    res.json(resultats);
+  });
+  
+  // POST : ajout d’un composant
+  router.post('/', (req, res) => {
+    const nouveauComposant = req.body;
+    composants.push(nouveauComposant);
+    res.status(201).json({ message: 'Composant ajouté', composant: nouveauComposant });
+  });
+  
+  // PUT : modification d’un composant
+  router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const index = composants.findIndex(c => c.id === id);
+  
+    if (index === -1) return res.status(404).json({ message: 'Composant non trouvé' });
+  
+    composants[index] = { ...composants[index], ...req.body };
+    res.json({ message: 'Composant mis à jour', composant: composants[index] });
+  });
+  
+  // DELETE : suppression d’un composant
+  router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    const index = composants.findIndex(c => c.id === id);
+  
+    if (index === -1) return res.status(404).json({ message: 'Composant non trouvé' });
+  
+    const supprimé = composants.splice(index, 1);
+    res.json({ message: 'Composant supprimé', composant: supprimé[0] });
+  });
+  
+  module.exports = router;
